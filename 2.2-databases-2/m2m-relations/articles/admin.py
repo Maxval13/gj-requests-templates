e4a3404ddadc78e5d498f2f49460d15306c5c1da
DMiheevskiy <maxval13Python@yandex.ru>
2023-05-21 18:13:26 +0300

    ORM2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+from .models import Article, Scope, Tag
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        is_main = False
+        for form in self.forms:
+            if form.cleaned_data and form.cleaned_data['is_main']:
+                is_main = True
+                count += 1
+        if not is_main:
+            raise ValidationError('Укажите основной раздел')
+        elif count > 1:
+            raise ValidationError('Основной раздел уже выбран')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'text', 'published_at', 'image']
+    inlines = [ScopeInline, ]
+    list_filter = ['tags']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -23,6 +23,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ['127.0.0.1',]
+
 
 SITE_ID = 1
 
@@ -37,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +81,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
